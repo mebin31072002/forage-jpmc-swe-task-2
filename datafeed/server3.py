16a550daaf4329ef09ba34dc831ddc74ce5d9a24
mebin31072002 <mebin002@gmail.com>
2023-11-09 12:24:33 +0530

    updated

--- datafeed/server3.py
+++ datafeed/server3.py
@@ -218,7 +218,7 @@ def get(req_handler, routes):
                 return
 
 
-def run(routes, host='192.168.1.6', port=3000):
+def run(routes, host='0.0.0.0', port=8080):
     """ Runs a class as a server whose methods have been decorated with
         @route.
     """
@@ -234,7 +234,7 @@ def run(routes, host='192.168.1.6', port=3000):
     thread = threading.Thread(target=server.serve_forever)
     thread.daemon = True
     thread.start()
-    print('HTTP server started on port 3000')
+    print('HTTP server started on port 8080')
     while True:
         from time import sleep
         sleep(1)
